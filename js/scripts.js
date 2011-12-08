ddb398ad72c74673535dcfd03c151742fc7150d7
David Ashkas <david@ashkas.com>
2011-12-08 15:17:56 +0900

    index js
    Change the JS to add in code for developing an index of items in the slideshow.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
 	$("#slideshow").css("overflow", "hidden");
 	
-	$("ul#slides").cycle({
+	//Create an array of titles
+	var titles = $('#slideshow div.item').find("img").map(function() { return $(this).text(); });
+	
+	//Add an unordered list to contain the navigation
+	$('#slideshow').before('<ul id="slides"></ul>').cycle({
 		fx: 'none', // Sets the transition to none
 		pause: 0, // Pause timer
 		timeout: 0, // Removes auto sliding to next slide
 		fit: 'true',     // force slides to fit container 
 		height: '100%', // Sets the height
 		containerResize: 0,   // resize container to fit largest slide
-		prev: '#prev', // force slides to fit container 
-		next: '#next' // force slides to fit container 
+		pager: '#slides', //Selector for element to use as pager container
+		pagerAnchorBuilder: function (index) {               //Build the pager
+            return '<li><a href="#">' + titles[index] + '</a></li>';
+            },
+		updateActivePagerLink: function(pager, currSlideIndex) { 
+    		$(pager).find('li').removeClass('active').filter('li:eq('+currSlideIndex+')').addClass('active'); 
+		}
+
 	});
 	
 	$("#slideshow").hover(function() {
