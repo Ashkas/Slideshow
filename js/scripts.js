3d48234c4566d6e9788928d40ea5007f21e9a5a6
David Ashkas <david@ashkas.com>
2011-12-09 16:27:11 +0900

    changes to the script

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,30 @@
 $(document).ready(function() {
-	$("#slideshow").css("overflow", "hidden");
 	
-	//Create an array of titles
-	var titles = $('#slideshow div.item').find("h1").map(function() { return $(this).text(); });
-	
-	//Add an unordered list to contain the navigation
-	$('#slideshow').before('<ul id="slides"></ul>').cycle({
-		fx: 'none', // Sets the transition to none
-		pause: 0, // Pause timer
-		timeout: 0, // Removes auto sliding to next slide
-		fit: 'true',     // force slides to fit container 
-		height: '100%', // Sets the height
-		containerResize: 0,   // resize container to fit largest slide
-		pager: '#slides', //Selector for element to use as pager container
-		pagerAnchorBuilder: function (index) {               //Build the pager
-            return '<li><a href="#">' + titles[index] + '</a></li>';
-            },
-		updateActivePagerLink: function(pager, currSlideIndex) { 
-    		$(pager).find('li').removeClass('active').filter('li:eq('+currSlideIndex+')').addClass('active'); 
-		}
+	$('#slideshow').cycle({
+            fx: 'none',
+            speed: 0,
+            timeout: 0,
+            pause: 0,
+            cleartype: true,
+            cleartypeNoBg: true,
+            fit: 'true',     // force slides to fit container 
+			height: '100%', // Sets the height
+			containerResize: 0,   // resize container to fit largest slide
+            pager: '#slides',
+            pagerAnchorBuilder: function(idx, slide) {
+                return '#slides li:eq(' + (idx) + ')';
+            }
+        });
 
-	});
-	
+        $('#slides-nav li').click(function() { 
+            $('#slideshow').cycle('pause'); 
+        });
+
+        $.fn.cycle.updateActivePagerLink = function(pager, currSlideIndex) { 
+            $(pager).find('li').removeClass('activeLI') 
+                .filter('li:eq('+currSlideIndex+')').addClass('activeLI'); 
+        }; 
+		
 	$("#slideshow").hover(function() {
     	$("ul#nav").fadeIn();
   	},
